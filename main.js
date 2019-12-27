4fb2e7227d9e19a4b9b7cc9ff26de3d2d0711bf7
DPannequin <dimitri@pannequin.fr>
2019-12-27 19:13:30 +0100

    Ajout d'un linter

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
 const client = new Client({ disableEveryone: true });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
+client.on("message", msg => {
+  if (msg.content === "ping") {
     msg.channel.send("Pong !");
   }
-  if (msg.content === 'everyone') {
-    msg.channel.send("@everyone, salut à tous !", { disableEveryone : false });
+  if (msg.content === "everyone") {
+    msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
   }
-  if (msg.content === 'noteveryone') {
+  if (msg.content === "noteveryone") {
     msg.channel.send("@everyone (noteveryone), salut à tous !");
   }
 });
 
-client.login('NjYwMTQ3NDgwMDc1NDM2MDMy.XgYpag.C3L6L1d1MpA8IpmOWXu0C4VB0AI');
+client.login("NjYwMTQ3NDgwMDc1NDM2MDMy.XgYpag.C3L6L1d1MpA8IpmOWXu0C4VB0AI");
