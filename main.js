a7b9c02c726062844c6c39ea4079f4b989faefb9
DPannequin <dimitri@pannequin.fr>
2019-12-29 15:43:27 +0100

    Optimisation gestion et variables commandes

--- main.js
+++ main.js
@@ -6,17 +6,20 @@ client.on("message", msg => {
   if (msg.author.bot) {
     return;
   }
-  const args = msg.content.split(/ +/g);
+  if (msg.content.indexOf(PREFIX) !== 0) {
+    return;
+  }
+  const args = msg.content.slice(PREFIX.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
 
-  if (cmd === `${PREFIX}ping`) {
+  if (cmd === "ping") {
     msg.channel.send("Pong !");
   }
-  if (cmd === `${PREFIX}repeat`) {
+  if (cmd === "repeat") {
     msg.channel.send(args.join(" "));
     msg.delete({ timeout: 3000 }).then(msg => console.log(`Un message de ${msg.author.username} a été supprimé : ${msg.content}`));
   }
-  if (cmd === `${PREFIX}role`) {
+  if (cmd === "role") {
     const role = msg.guild.roles.find(r => r.name === args[0]);
     const channel_logs = client.channels.find(r => r.name === "logs");
     if (!role) {
@@ -26,8 +29,7 @@ client.on("message", msg => {
       msg.member.roles.remove(role);
       channel_logs.send(`J'ai supprimé le rôle ${role} à ${msg.author}.`);
       msg.delete({ timeout: 3000 });
-    }
-    else {
+    } else {
       msg.member.roles.add(role);
       channel_logs.send(`J'ai ajouté le rôle ${role} à ${msg.author}.`);
       msg.delete({ timeout: 3000 });
